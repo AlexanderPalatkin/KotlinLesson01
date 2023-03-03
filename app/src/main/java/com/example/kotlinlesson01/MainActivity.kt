300f1cf8f9d3d2efe5f14aad32f1af2ffb115567
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-03-03 17:32:00 +0400

    Добавил список Int на котором тестировал работу циклов и if, when. Весь тест в методе testCycle

--- app/src/main/java/com/example/kotlinlesson01/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinlesson01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val arrayTest = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+        testCycle(arrayTest)
+
         val car1 = Car("bmw", "auto")
         val car2 = car1.copy(name = "mers", speed = 150)
 
@@ -17,7 +21,30 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             findViewById<TextView>(R.id.tvTest).text = "$car1 \n $car2"
         }
+
     }
 
     data class Car(val name: String, val type: String, val speed: Int = 100)
+
+    private fun testCycle(array: List<Int>) {
+        for (i in array) {
+            if (i < 6) Log.d("myLogs", "$i") else Log.d("myLogs", "$i >= 6")
+        }
+
+        for (i in array.indices) {
+            when(i) {
+                2 -> Log.d("myLogs", "$i")
+                5 -> Log.d("myLogs", "$i")
+
+            }
+        }
+
+        for (i in 1..3) {
+            Log.d("myLogs", "$i")
+        }
+
+        repeat(3) {
+            Log.d("myLogs", "Boo")
+        }
+    }
 }
