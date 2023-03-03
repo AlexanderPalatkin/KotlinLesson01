e42cf57fc5ed0d4240b9b6802587ce47fd31cda3
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-03-03 17:05:14 +0400

    Добавил data class Car. Создал экземпляр этого класса и его копию с изменением имени и скорости. Добавил вывод на экран этих 2 эземпляров при нажатии на кнопку button

--- app/src/main/java/com/example/kotlinlesson01/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson01/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.kotlinlesson01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.kotlinlesson01.R
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val car1 = Car("bmw", "auto")
+        val car2 = car1.copy(name = "mers", speed = 150)
+
         val button = findViewById<Button>(R.id.bTest)
         button.setOnClickListener {
-
+            findViewById<TextView>(R.id.tvTest).text = "$car1 \n $car2"
         }
     }
+
+    data class Car(val name: String, val type: String, val speed: Int = 100)
 }
